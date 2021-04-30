6e067920b1e2ab29f89de5e45530a1979aec8e52
joaquin <jecorrea@protonmail.com>
2021-04-29 20:41:18 -0400

    commit tree 20f6a79fb1f04128b03647e3c9b522921c07aa74

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+const node = document.getElementById("text");
+
+    document.addEventListener("DOMContentLoaded", function() {
+     node.textContent = `This is really cool!`;}
+    );
+
